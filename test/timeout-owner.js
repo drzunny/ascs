12a7d65a8b9d98015093fa4e531bb3a352b827d3
drzunny <drzunny@hotmail.com>
2014-04-08 01:13:27 +0800

    update test

--- test/timeout-owner.js
+++ test/timeout-owner.js
@@ -18,6 +18,30 @@ var expect  = chai.expect;
 
 /*------------------ The test case ------------------*/
 describe("timeout-owner-scope", function () {
+    var fs = require("fs");
+
+    //  Prepare the basic function
+    //--------------------------------------------------
+    var fun1 = fs.exists;
+    function fun2 ()    {
+    };
+    function fun ()    {
+        this.fun3 = function () {
+        };
+    };
+    function fun.fun4() {
+    };
+    function fun5 ()    {
+    };
+
+    var fun1_async = ascs.make(fun1);
+    var fun2_async = ascs.make(fun2);
+    var fun3_async = ascs.make(f.fun3);
+    var fun4_async = ascs.make(fun.fun4);
+    var fun5_async = ascs.make(fun5);
+
+    //  Start the mocha test
+    //--------------------------------------------------
     describe("await-timeout", function () {
     });
 
